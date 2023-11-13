56a642b9e9f91bec5ee10c92b9db7ff48a83b92e
zeeeunee <wldmstv@gmail.com>
2023-11-13 18:20:53 +0900

    미션4

--- js/main.js
+++ js/main.js
@@ -8,8 +8,12 @@ btns.forEach((btn, idx) => {
 	});
 });
 
-btns.forEach((btn) => {
+btns.forEach((btn, idx) => {
 	btn.addEventListener('click', (e) => {
 		box.style.backgroundColor = e.currentTarget.innerText;
+
+		if (e.currentTarget.classList.contains('on')) return;
+		btns.forEach((btn) => btn.classList.remove('on'));
+		btns[idx].classList.add('on');
 	});
 });
