6c49319d58ef70b3936753676d26c0f1cdcb3afe
zeeeunee <wldmstv@gmail.com>
2023-11-13 17:51:57 +0900

    미션2

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+const box = document.querySelector('.box');
 const btns = document.querySelectorAll('.btns li');
 
 btns.forEach((btn, idx) => {
@@ -5,4 +6,14 @@ btns.forEach((btn, idx) => {
 		btns.forEach((btn) => btn.classList.remove('on'));
 		btns[idx].classList.add('on');
 	});
+});
+
+btns[0].addEventListener('click', () => {
+	box.style.backgroundColor = 'hotpink';
+});
+btns[1].addEventListener('click', () => {
+	box.style.backgroundColor = 'royalblue';
+});
+btns[2].addEventListener('click', () => {
+	box.style.backgroundColor = 'orange';
 });
